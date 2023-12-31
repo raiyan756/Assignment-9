d6cf819c29b4776ecb178760f7c119b4f91c9170
raiyan756 <raiyanmugdho6@gmail.com>
2023-12-31 11:01:30 +0600

    last commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const news=useLoaderData();
         <div>
             <h1 className='text-center text-6xl'>This Is Sports Carnival</h1>
             <br />
-            <h1 className='text-center'>The contents of this course are solidified by our world-class faculty and elusive network of sport people and associations. Learners will gain a holistic understanding of the sport event landscape and become confident in their ability to think and converse about sport event management.</h1>
+            <h1 className='text-center'>The contents of this course are class faculty and elusive network of sport people and associations. Learners will gain a holistic understanding of the sport event landscape and become confident in their ability to think and converse about sport event management.</h1>
         <Navbar className='text-center' ></Navbar>
         <div className='lg:flex sm:block'>
             <div><Leftside></Leftside></div>
