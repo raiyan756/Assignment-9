17f0fa9318dcd782d70d02730b74b134ca7a7fee
raiyan756 <raiyanmugdho6@gmail.com>
2023-12-31 11:11:26 +0600

    firsint commit

--- src/Pages/Leftside.jsx
+++ src/Pages/Leftside.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Leftside = () => {
     return (
         <div>
-            <h1 className='border-2 col-span-3'>this is left side</h1>
+            <h1 className='border-2 col-span-3'>this is side</h1>
         </div>
     );
 };
